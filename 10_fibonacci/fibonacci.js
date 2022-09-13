adbc0d73c64bb34600b3e1e72763eee12daa2071
Jack Agtual <jaagtual@gmail.com>
2022-09-12 22:30:18 -0700

    Exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(idx) {
+    idx = Number(idx);
+    if (idx < 0) return 'OOPS';
+    if (idx <= 2) return 1;
 
+    let prev1 = 1;
+    let prev2 = 1;
+    let curSum;
+    
+    for (let i = 3; i <= idx; i++) {
+        curSum = prev1 + prev2;
+        prev1 = prev2;
+        prev2 = curSum;
+    }
+    return curSum;
 };
 
 // Do not edit below this line
