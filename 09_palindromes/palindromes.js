46955dfd23cfc90ec2a3e661f0e9f8580ad83fbd
Jack Agtual <jaagtual@gmail.com>
2022-09-12 22:02:13 -0700

    Exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,25 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    str = removePunctuation(str).toLowerCase();
+    console.log(str);
 
+    if (str === reverseStr(str)) return true;
+    return false;
 };
 
+const reverseStr = str => {
+    return str.split('').reverse().join('');
+}
+
+const removePunctuation = str => {
+    // remove any punctuation and combine string
+    const replacements = ['!', ',', ' ', '.'];
+
+    replacements.forEach(el => {
+        str = str.replaceAll(el, '');
+    })
+    return str;
+}
+
+palindromes('A car, a man, a maraca.')
 // Do not edit below this line
 module.exports = palindromes;
