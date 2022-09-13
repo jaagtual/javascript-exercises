a94967f37b35550653e193d8127c6e43306f62c6
Jack Agtual <jaagtual@gmail.com>
2022-09-12 20:50:46 -0700

    Exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(array) {
+    // instead of variable number of arguments, I'd make the second parameter an array of values to remove
 
+    for (let i = 1; i < arguments.length; i++){
+        array = array.filter(el => el !== arguments[i]);
+    }
+
+    return array;
 };
 
 // Do not edit below this line
