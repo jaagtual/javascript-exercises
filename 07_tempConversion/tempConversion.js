dd3e9b6e3eec52fbb9ac07780e97ca220e7aa88e
Jack Agtual <jaagtual@gmail.com>
2022-09-12 21:14:38 -0700

    Exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,9 @@
-const ftoc = function() {
-
+const ftoc = function(tempF) {
+  return Math.round(10 * ((tempF - 32) * 5/9)) / 10;
 };
 
-const ctof = function() {
-
+const ctof = function(tempC) {
+  return Math.round(10 * (tempC * 9/5 + 32)) / 10;
 };
 
 // Do not edit below this line
