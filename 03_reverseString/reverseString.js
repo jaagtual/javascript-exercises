a0f906b916032ff540a0a84d7f1f85607339c5b4
Jack Agtual <jaagtual@gmail.com>
2022-09-12 20:38:20 -0700

    Exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,12 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    let newStr = '';
 
+    for (let i = str.length - 1; i >= 0; i--){
+        newStr += str.charAt(i);
+    }
+    return newStr;
 };
 
+reverseString('hello')
 // Do not edit below this line
 module.exports = reverseString;
