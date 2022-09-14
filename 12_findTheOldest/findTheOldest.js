12b20876ef6cb213c54ddc24734f6181c42029d8
Jack Agtual <jaagtual@gmail.com>
2022-09-13 21:08:44 -0700

    Exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    const CUR_YEAR = 2022;
+    let maxAge = -1;
+    let oldestPerson;
+    people.forEach(person => {
+        let maxYear = person.yearOfDeath ? person.yearOfDeath : CUR_YEAR;
+        let age = maxYear - person.yearOfBirth;
+        if (maxYear - person.yearOfBirth > maxAge){
+            oldestPerson = person;
+            maxAge = age;
+        }
+    })
+    return oldestPerson;
 };
 
 // Do not edit below this line
