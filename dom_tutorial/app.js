190caadff8210d6fc54a4dd3f76b3770fff1de36
Jack Agtual <jaagtual@gmail.com>
2022-09-13 22:05:03 -0700

    DOM Exercise: Adding more elements to dom

---
+++ dom_tutorial/app.js
@@ -0,0 +1,27 @@
+const container = document.querySelector('#container');
+
+const p = document.createElement('p');
+p.innerText = `Hey I'm red`;
+p.style['color'] = 'red';
+container.appendChild(p)
+
+const h3 = document.createElement('h3');
+h3.innerText = `I'm a blue h3!`;
+h3.style['color'] = 'blue';
+container.appendChild(h3);
+
+const div = document.createElement('div');
+div.style['background-color'] = 'pink';
+div.style['border'] = '2px solid black';
+
+const h1Div = document.createElement('h1');
+h1Div.innerText = `I'm in a div!`
+div.appendChild(h1Div);
+
+const pDiv = document.createElement('p');
+pDiv.innerText = `ME TOO!`;
+div.appendChild(pDiv);
+
+container.appendChild(div);
+
+container.appendChild(div);
