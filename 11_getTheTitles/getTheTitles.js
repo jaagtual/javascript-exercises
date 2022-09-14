acd87bd134dd715fcd70f8f32b1410b4f5eabc92
Jack Agtual <jaagtual@gmail.com>
2022-09-13 20:25:11 -0700

    Exercise 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,10 @@
-const getTheTitles = function() {
+const getTheTitles = function(books) {
+    let titles = [];
 
+    books.forEach(obj => {
+        titles.push(obj.title);    
+    });
+    return titles;
 };
 
 // Do not edit below this line
